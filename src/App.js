a05b07a5bbbdc80904394a0677438ee5f09ba320
Ma. Su. Pirachanna <pirachanna0504@gmail.com>
2025-07-15 15:51:44 +0530

    Fix anchor navigation and remove React Router for GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,20 +1,13 @@
 import React from 'react';
-import { HashRouter, Routes, Route } from 'react-router-dom';
 import Index from './Index/Index';
 import AppEffects from './Components/Appeffects';
 
 function App() {
   return (
-    <HashRouter>
+    <>
       <AppEffects />
-      <Routes>
-        <Route path="/" element={<Index />} />
-        <Route path="/about" element={<Index />} />
-        <Route path="/contact" element={<Index />} />
-        <Route path="/services" element={<Index />} />
-        <Route path="/skill" element={<Index />} />
-      </Routes>
-    </HashRouter>
+      <Index />
+    </>
   );
 }
 
