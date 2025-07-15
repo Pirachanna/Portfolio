845d4884cc136b6987abad21ce892ba67b620181
Ma. Su. Pirachanna <pirachanna0504@gmail.com>
2025-07-15 10:36:33 +0530

    Switch to HashRouter for GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Index from './Index/Index';
-import AppEffects from './Components/Appeffects'; 
+import AppEffects from './Components/Appeffects';
 
 function App() {
   return (
-    <BrowserRouter>
-      <AppEffects /> 
+    <HashRouter>
+      <AppEffects />
       <Routes>
         <Route path='/' element={<Index />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
